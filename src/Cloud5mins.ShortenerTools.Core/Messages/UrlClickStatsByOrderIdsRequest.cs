6b05ed23170ad59622313c89b4a4951e11dbe018
hugo-rog <hugo@hugoforte.com>
2023-10-19 14:50:02 -0500

    Fixed she shortener

--- src/Cloud5mins.ShortenerTools.Core/Messages/UrlClickStatsByOrderIdsRequest.cs
+++ src/Cloud5mins.ShortenerTools.Core/Messages/UrlClickStatsByOrderIdsRequest.cs
@@ -2,10 +2,6 @@ namespace LettrLabs.UrlShorterner.Core.Messages
 {
     public class UrlClickStatsByOrderIdsRequest
     {
-        public UrlClickStatsByOrderIdsRequest()
-        {
-            OrderIds = new List<int>();
-        }
         public IList<int> OrderIds { get; }
 
         public UrlClickStatsByOrderIdsRequest(IList<int> orderIds)
